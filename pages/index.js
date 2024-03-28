507821aa6fde9f471cd7c92cf94abd0d03cd3888
yoga333 <542703630@qq.com>
2024-03-28 23:40:11 +0800

    index only show data_trans

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import DataConverter from './DataConverter';
-import JsonFormatter from './JsonFormatter';
+// import JsonFormatter from './JsonFormatter';
 import styles from './Home.module.css';
 
 export default function Home() {
@@ -14,11 +14,11 @@ export default function Home() {
         <div className={styles.container}>
             {/* <h1>请选择一个工具</h1> */}
             <div className={styles.buttons}> {/* 添加了这一行 */}
-                <button className={styles.button} onClick={() => handleClick('data-converter')}>大小转换</button>
-                <button className={styles.button} onClick={() => handleClick('json-formatter')}>JSON格式化</button>
+                {/* <button className={styles.button} onClick={() => handleClick('data-converter')}>大小转换</button> */}
+                {/* <button className={styles.button} onClick={() => handleClick('json-formatter')}>JSON格式化</button> */}
             </div> {/* 添加了这一行 */}
             {selectedTool === 'data-converter' && <DataConverter />}
-            {selectedTool === 'json-formatter' && <JsonFormatter />}
+            {/* {selectedTool === 'json-formatter' && <JsonFormatter />} */}
         </div>
     );
 }
